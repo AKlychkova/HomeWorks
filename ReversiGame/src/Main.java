0572589c5d7ebea1ac7a72f0b244e129b741e69c
AKlychkova <90353866+AKlychkova@users.noreply.github.com>
2022-12-05 01:31:36 +0300

    Delete ReversiGame directory

--- ReversiGame/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        do {
-            new Game(Output.getPlayersAmount()).start();
-        } while (Output.playAgain());
-    }
-}
